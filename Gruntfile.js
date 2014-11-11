6e2b70f6543b8eb872bcf77addf22edec1b8fbbd
Lucas Käldström <lucas.kaldstrom@gmail.com>
2014-11-11 12:43:27 +0200

    First commit, configured grunt

---
+++ Gruntfile.js
@@ -0,0 +1,97 @@
+//
+module.exports = function (grunt) {
+	
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			dev: {
+				options: {
+					compress: false,
+					beautify: true,
+					banner: "/*! <%= pkg.name %> - v<%= pkg.version %> - ' +'<%= grunt.template.today('yyyy-mm-dd') %> */"
+				},
+				files: {
+					'public/js/home.min.js': ['src/js/home.js']
+				}
+			},
+			dist: {
+				options: {
+					banner: "/*! <%= pkg.name %> - v<%= pkg.version %> - ' +'<%= grunt.template.today('yyyy-mm-dd') %> */"
+				},
+				files: {
+					'public/js/home.min.js': ['src/js/home.js']
+				}
+			}
+		},
+		sass: {
+			dev: {
+				options: {
+					style: "expanded",
+					banner: "/*! <%= pkg.name %> - v<%= pkg.version %> - ' +'<%= grunt.template.today('yyyy-mm-dd') %> */"
+				},
+				files: {
+					'public/css/home.css': 'src/sass/home.scss'
+				}
+			},
+			dist: {
+				options: {
+					style: "compressed",
+					banner: "/*! <%= pkg.name %> - v<%= pkg.version %> - ' +'<%= grunt.template.today('yyyy-mm-dd') %> */"
+				},
+				files: {
+					'public/css/home.css': 'src/sass/home.scss'
+				}
+			}
+		},
+		imagemin: {
+			dev: {
+				files: [{
+			        expand: true,                  // Enable dynamic expansion
+			        cwd: 'src/img/',                   // Src matches are relative to this path
+			        src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+			        dest: 'public/img/'                  // Destination path prefix
+			    }]
+			},
+			dist: {
+				files: [{
+			        expand: true,                  		// Enable dynamic expansion
+			        cwd: 'src/img/',                   	// Src matches are relative to this path
+			        src: ['**/*.{png,jpg,gif}'],   		// Actual patterns to match
+			        dest: 'public/img/'                 // Destination path prefix
+			    }]
+			}
+		},
+		watch: {
+			me: {
+			    files: ['Gruntfile.js'],
+			    options: {
+			      reload: true
+			    }
+			},
+			js:  {
+				files: ["src/js/*.js"],
+				tasks: ["uglify:<% pkg.debug ? 'dev' : 'dist' %>"]
+			},
+			sass: {
+				files: ["src/sass/*.sass"],
+				tasks: ["sass:<% pkg.debug ? 'dev' : 'dist' %>"]
+			},
+			img: {
+				files: ["src/img/*.{png,jpg,gif}"],
+				tasks: ["imagemin:<% pkg.debug ? 'dev' : 'dist' %>"]
+			}
+		}
+	})
+
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	// this would be run by typing "grunt test" on the command line
+	grunt.registerTask('dev', ['uglify:dev', 'sass:dev', "imagemin:dev", "watch"]);
+
+	// the default task can be run just by typing "grunt" on the command line
+	grunt.registerTask('default', ['uglify:dist', 'sass:dist', "imagemin:dist", "watch"]);
+}
